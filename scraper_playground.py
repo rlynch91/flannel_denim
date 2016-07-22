fe16af87c93219f52243d8c694388ca97174b05d
Ryan Lynch <ryan.lynch@ligo.org>
2016-07-22 02:09:29 -0400

    Scraping script now gets both hockey-reference.com and NHL.com data for a given game

--- scraper_playground.py
+++ scraper_playground.py
@@ -2,14 +2,24 @@ from bs4 import BeautifulSoup
 import requests
 import pickle
 import numpy as np
+from selenium import webdriver
 
 #-----------------------------------------------------------------------
 
-away_team = 'CHI'
-home_team = 'MIN'
-date = '2013'+'01'+'30'
+#Initialize variables
 
-page = requests.get("http://www.hockey-reference.com/boxscores/%s0%s.html"%(date,home_team))
+year = '2016'
+month = '03'
+day = '20'
+away_team = 'MIN'
+home_team = 'CHI'
+home_team_long = 'blackhawks'
+
+#-----------------------------------------------------------------------
+
+#Hockey-reference.com box scores
+
+page = requests.get("http://www.hockey-reference.com/boxscores/%s0%s.html"%(year+month+day,home_team))
 soup = BeautifulSoup(page.text)
 
 scores = []
@@ -59,3 +69,45 @@ home_adv_table = home_adv_soup.find("table", attrs={"id":"%s_adv"%home_team})
 home_adv_headers = [row.get_text().strip() for row in home_adv_table.find("thead").find_all("tr")]
 home_adv_stats = [row.get_text().strip() for row in home_adv_table.find("tbody").find_all("tr",attrs={"class":"All-ALL"})]
 home_adv_players = [row.find('a')['href'] for row in home_adv_table.find("tbody").find_all("tr",attrs={"class":"All-ALL"})]
+
+#-----------------------------------------------------------------------
+
+#NHL.com box scores
+
+schedule_page = requests.get("https://www.nhl.com/%s/schedule/%s/ET"%(home_team_long,year+'-'+month+'-'+day))
+schedule_soup = BeautifulSoup(schedule_page.text)
+nhl_game_num = schedule_soup.find("a", attrs={"title":"Recap"})['href'].split('/')[-1]
+
+driver = webdriver.PhantomJS()
+driver.get("https://www.nhl.com/gamecenter/%s-vs-%s/%s/%s/%s/%s#game=%s,game_state=final,game_tab=boxscore"%(away_team.lower(),home_team.lower(),year,month,day,nhl_game_num,nhl_game_num))
+soup = BeautifulSoup(driver.page_source)
+
+team_table = soup.find("div",attrs={"class":"statistics__season-stats"})
+nhl_team_headers = team_table.find("thead").find("tr").get_text().strip().split()
+nhl_team_stats = [row.get_text().split() for row in team_table.find("tbody").find_all("tr")]
+away_nhl_team_stats = nhl_team_stats[0]
+home_nhl_team_stats = nhl_team_stats[1]
+
+away_nhl_headers = []
+away_nhl_stats = []
+away_nhl_players = []
+away_tables = soup.find("div", attrs={"class":"away"}).find_all("div",attrs={"class":"responsive-datatable"})
+for away_table in away_tables:
+	away_nhl_headers += [away_table.find("thead").find("tr").get_text().strip().split()]
+	away_nhl_rows = away_table.find("tbody").find_all("tr")
+	for row in away_nhl_rows:
+		away_nhl_stats += [[datum.get_text().strip() for datum in row.find_all('td')]]
+		away_nhl_players += [" ".join(row.find('a')['href'].split('/player/')[-1].split('-')[0:2]).title()]
+
+home_nhl_headers = []
+home_nhl_stats = []
+home_nhl_players = []
+home_tables = soup.find("div", attrs={"class":"home"}).find_all("div",attrs={"class":"responsive-datatable"})
+for home_table in home_tables:
+	home_nhl_headers += [home_table.find("thead").find("tr").get_text().strip().split()]
+	home_nhl_rows = home_table.find("tbody").find_all("tr")
+	for row in home_nhl_rows:
+		home_nhl_stats += [[datum.get_text().strip() for datum in row.find_all('td')]]
+		home_nhl_players += [" ".join(row.find('a')['href'].split('/player/')[-1].split('-')[0:2]).title()]
+
+driver.quit()
