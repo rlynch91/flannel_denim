2c944d1e8f7355fce6d834163c0f5d9c2faf2f4a
Ryan Lynch <ryan.lynch@ligo.org>
2016-07-25 01:20:10 -0400

    Added vegas odds to scraper, working on scraped dictionary organization

--- scraper_playground.py
+++ scraper_playground.py
@@ -3,6 +3,7 @@ import requests
 import pickle
 import numpy as np
 from selenium import webdriver
+import unicodedata
 
 #-----------------------------------------------------------------------
 
@@ -14,6 +15,24 @@ day = '20'
 away_team = 'MIN'
 home_team = 'CHI'
 home_team_long = 'blackhawks'
+home_team_city = "Chicago"
+
+#-----------------------------------------------------------------------
+
+#Initialize dictionary in which to store everything
+dictionary = {}
+
+dictionary['date'] = {}
+dictionary['date']['day'] = day
+dictionary['date']['month'] = month
+dictionary['date']['year'] = year
+
+dictionary['teams'] = {}
+dictionary['teams'][away_team] = {}
+dictionary['teams'][away_team]['location'] = 'away'
+
+dictionary['teams'][home_team] = {}
+dictionary['teams'][home_team]['location'] = 'home'
 
 #-----------------------------------------------------------------------
 
@@ -31,7 +50,7 @@ for i,div in enumerate(elements):
 		if div['class'][0] == 'score':
 			scores += [elements[i].get_text().strip()]
 			streaks += [elements[i+1].get_text().strip()]
-			
+
 away_heading = soup.find("div", attrs={"id":"all_%s_skaters"%away_team})
 away_record_after = away_heading.find('h2').get_text().strip().split()[-1]
 
@@ -69,6 +88,80 @@ home_adv_table = home_adv_soup.find("table", attrs={"id":"%s_adv"%home_team})
 home_adv_headers = [row.get_text().strip() for row in home_adv_table.find("thead").find_all("tr")]
 home_adv_stats = [row.get_text().strip() for row in home_adv_table.find("tbody").find_all("tr",attrs={"class":"All-ALL"})]
 home_adv_players = [row.find('a')['href'] for row in home_adv_table.find("tbody").find_all("tr",attrs={"class":"All-ALL"})]
+
+
+#Update dictionary
+dictionary['teams'][away_team]['goals for'] = int(scores[0])
+dictionary['teams'][away_team]['goals against'] = int(scores[1])
+dictionary['teams'][home_team]['goals for'] = int(scores[1])
+dictionary['teams'][home_team]['goals against'] = int(scores[0])
+
+dictionary['teams'][away_team]['players'] = {}
+for (player,stats) in zip(away_skaters_players,away_skaters_stats):
+	dictionary['teams'][away_team]['players'][player] = {}
+	headers = away_skaters_headers.split()
+	stats = stats.split()
+	
+	player_name = ""
+	i_name = 1
+	while True:
+		try:
+			__ = float(stats[i_name])
+			break
+		except ValueError:
+			player_name += str(stats[i_name]) + " "
+			i_name += 1
+	dictionary['teams'][away_team]['players'][player][str(headers[1])] = player_name.strip()
+	for i in np.arange(2,7):
+		dictionary['teams'][away_team]['players'][player][str(headers[i])] = float(stats[i+i_name-2])
+	for i in np.arange(7,11):
+		dictionary['teams'][away_team]['players'][player][str(headers[i])+' goals'] = float(stats[i+i_name-2])
+	for i in np.arange(11,14):
+		dictionary['teams'][away_team]['players'][player][str(headers[i])+' assists'] = float(stats[i+i_name-2])
+	dictionary['teams'][away_team]['players'][player][str(headers[14])] = float(stats[14+i_name-2])
+	if len(stats) == 18:
+		dictionary['teams'][away_team]['players'][player][str(headers[15])] = np.nan
+		dictionary['teams'][away_team]['players'][player][str(headers[16])] = float(stats[16+i_name-3])
+	elif len(stats) == 19:
+		dictionary['teams'][away_team]['players'][player][str(headers[15])] = float(stats[15+i_name-2])
+		dictionary['teams'][away_team]['players'][player][str(headers[16])] = float(stats[16+i_name-2])
+	dictionary['teams'][away_team]['players'][player][str(headers[-1])] = str(stats[-1])
+
+dictionary['teams'][home_team]['players'] = {}
+for (player,stats) in zip(home_skaters_players,home_skaters_stats):
+	dictionary['teams'][home_team]['players'][player] = {}
+	headers = home_skaters_headers.split()
+	stats = stats.split()
+	
+	player_name = ""
+	i_name = 1
+	while True:
+		try:
+			__ = float(stats[i_name])
+			break
+		except ValueError:
+			player_name += str(stats[i_name]) + " "
+			i_name += 1
+	dictionary['teams'][home_team]['players'][player][str(headers[1])] = player_name.strip()
+	for i in np.arange(2,7):
+		dictionary['teams'][home_team]['players'][player][str(headers[i])] = float(stats[i+i_name-2])
+	for i in np.arange(7,11):
+		dictionary['teams'][home_team]['players'][player][str(headers[i])+' goals'] = float(stats[i+i_name-2])
+	for i in np.arange(11,14):
+		dictionary['teams'][home_team]['players'][player][str(headers[i])+' assists'] = float(stats[i+i_name-2])
+	dictionary['teams'][home_team]['players'][player][str(headers[14])] = float(stats[14+i_name-2])
+	if len(stats) == 18:
+		dictionary['teams'][home_team]['players'][player][str(headers[15])] = np.nan
+		dictionary['teams'][home_team]['players'][player][str(headers[16])] = float(stats[16+i_name-3])
+	elif len(stats) == 19:
+		dictionary['teams'][home_team]['players'][player][str(headers[15])] = float(stats[15+i_name-2])
+		dictionary['teams'][home_team]['players'][player][str(headers[16])] = float(stats[16+i_name-2])
+	dictionary['teams'][home_team]['players'][player][str(headers[-1])] = str(stats[-1])
+
+#Get goalie info, parse for game decision
+#Link advanced stats to each player
+#Use decision to find team record before game
+	
 
 #-----------------------------------------------------------------------
 
@@ -109,5 +202,44 @@ for home_table in home_tables:
 	for row in home_nhl_rows:
 		home_nhl_stats += [[datum.get_text().strip() for datum in row.find_all('td')]]
 		home_nhl_players += [" ".join(row.find('a')['href'].split('/player/')[-1].split('-')[0:2]).title()]
+
+#-----------------------------------------------------------------------
+
+#Sportsbookreview.com vegas odds
+
+driver.get('http://www.sportsbookreview.com/betting-odds/nhl-hockey/?date=%s%s%s'%(year,month,day))
+soup = BeautifulSoup(driver.page_source)
+
+team_name_soup = soup.find_all("span",attrs={"class":"team-name"})
+for team in team_name_soup:
+	if team.get_text() == home_team_city:
+		game_number = team.find("a")['href'].split('-')[-1].strip('/')
+		break
+
+moneylines = []
+team_lines = soup.find_all("div", attrs={"class":"eventLine-book-value"})
+i_team = 0	
+for line in team_lines:
+	if line.has_attr('id'):
+		if line['id'].split('-')[1] == game_number and line['id'].split('-')[2] == '1096':
+			moneylines += [line.get_text()]
+			if i_team >= 1:
+				break
+			else:
+				i_team += 1
+				
+away_moneylines = moneylines[0]	
+home_moneylines = moneylines[1]
+
+driver.get('http://www.sportsbookreview.com/betting-odds/nhl-hockey/totals/?date=%s%s%s'%(year,month,day))
+soup = BeautifulSoup(driver.page_source)		
+
+total_number = 0.
+for i in soup.find("div", attrs={"id":"eventLineOpener-%s-1096-o-3"%game_number}).find("span",attrs={"class":"adjust"}).get_text():
+	total_number += unicodedata.numeric(i)
+total_line_over = soup.find("div", attrs={"id":"eventLineOpener-%s-1096-o-3"%game_number}).find("span",attrs={"class":"price"}).get_text()
+total_line_under = soup.find("div", attrs={"id":"eventLineOpener-%s-1096-u-3"%game_number}).find("span",attrs={"class":"price"}).get_text()
+
+#-----------------------------------------------------------------------
 
 driver.quit()
